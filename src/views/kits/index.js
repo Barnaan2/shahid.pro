2e1f13d4508a04703e4ba3becf56beb05495425d
sh4hids <hello@shahid.pro>
2019-04-12 09:24:34 +0600

    Deleted *

--- src/views/kits/index.js
+++
@@ -1,8 +0,0 @@
-import Container from './container';
-import Text from './text';
-import URL from './url';
-import List from './list';
-import ListItem from './list-item';
-import Image from './image';
-
-export { Container, Text, URL, List, ListItem, Image };
